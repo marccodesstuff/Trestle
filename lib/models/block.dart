bdf7c5c66c961c8c9bfd68fee24114f56e633bff
Marc Velasquez <104043486+marccodesstuff@users.noreply.github.com>
2024-12-07 20:33:07 +0800

    Update block.dart

--- lib/models/block.dart
+++ lib/models/block.dart
@@ -1,12 +1,28 @@
 class Block {
-  String id;
+  final String id;
+  BlockType type;
   String content;
+  BlockAlignment alignment;
   String? imageUrl;
   double? width;
-  BlockType type;
-  BlockAlignment alignment;
 
-  Block({required this.id, required this.content, this.imageUrl, this.width, required this.type, this.alignment = BlockAlignment.left});
+  Block({
+    required this.id,
+    required this.content,
+    this.imageUrl,
+    this.width,
+    required this.type,
+    this.alignment = BlockAlignment.left
+  });
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'content': content,
+    'imageUrl': imageUrl,
+    'width': width,
+    'type': type.toString().split('.').last,
+    'alignment': alignment.toString().split('.').last,
+  };
 }
 
 enum BlockType {
