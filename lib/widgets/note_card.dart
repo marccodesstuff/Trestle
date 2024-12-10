bd3648987791fa65fd6be1eb5cae93a69f76bfb1
Marc Velasquez <104043486+marccodesstuff@users.noreply.github.com>
2024-12-11 05:35:50 +0800

    Added navigation to the document cards

--- lib/widgets/note_card.dart
+++ lib/widgets/note_card.dart
@@ -2,26 +2,30 @@ import 'package:flutter/material.dart';
 
 class NoteCard extends StatelessWidget {
   final String title;
+  final VoidCallback onTap;
 
-  const NoteCard({super.key, required this.title});
+  const NoteCard({super.key, required this.title, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ],
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
         ),
       ),
     );
