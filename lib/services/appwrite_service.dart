b48ed50d43b2183a5e1c5e6a5e525337be6571fd
Marc Velasquez <104043486+marccodesstuff@users.noreply.github.com>
2024-12-11 02:10:40 +0800

    Added a save function for existing documents
    It is the same as `saveNewDocuments`, but with a different name. Function is subjected to changes.

--- lib/services/appwrite_service.dart
+++ lib/services/appwrite_service.dart
@@ -63,6 +63,44 @@ class AppWriteService {
     }
   }
 
+  // I'll figure out how to implement this later
+  Future<void> saveOldDocument(String documentTitle, List<Widget> blocks) async {
+    final client = Client().setEndpoint('https://cloud.appwrite.io/v1').setProject('675448ef003e37a9488a');
+    final _databases = Databases(client);
+    try {
+      print('Starting to save document: $documentTitle');
+
+      // Convert blocks to a format suitable for storage
+      List<Map<String, dynamic>> blockData = blocks.map<Map<String, dynamic>>((block) {
+        if (block is TextField) {
+          return {
+            'type': 'text',
+            'content': (block.controller as TextEditingController).text,
+          };
+        } else if (block is ImageBlock) {
+          return {
+            'type': 'image',
+            'url': (block as ImageBlock).imageUrl,
+          };
+        }
+        return {};
+      }).toList();
+
+      // Convert block data to JSON string
+      String blockDataJson = jsonEncode(blockData);
+      print('Block data JSON: $blockDataJson');
+
+      try {
+        await _databases.createDocument(
+          databaseId: 'trestle_notes',
+          collectionId: 'trestle_docs',
+          documentId: ID.unique(), // Generate a unique ID
+          data: {
+            'id': ID.unique().toString(),
+            'title': documentTitle,
+            'content': blockDataJson,
+            'date_created': DateTime.now().toIso8601String(),
+            'date_updated': DateTime.now().toIso8601String(),
           },
         );
       } catch (e) {
